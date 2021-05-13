61f300596c9899ac75ec801a6557af03eaf277c4
andrey <andreika.varfolomeev@yandex.ru>
2021-05-14 02:23:32 +1000

    fix TELEGRAM_FATHER_ID

--- amigo/env.py
+++ amigo/env.py
@@ -2,7 +2,7 @@ import os
 
 # Telegram
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_FATHER_ID = int(os.getenv('TELEGRAM_FATHER_ID'))
+TELEGRAM_FATHER_ID = int(os.getenv('TELEGRAM_FATHER_ID') or 0)
 
 # Database
 DATABASE_URL = os.getenv('DATABASE_URL') or 'sqlite:///database.db'
