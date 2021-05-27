eca7ccacd871a0cb2b0d55649df8895e77be20bc
andrey <andreika.varfolomeev@yandex.ru>
2021-05-28 02:02:12 +1000

    improve help

--- amigo/views/help.py
+++ amigo/views/help.py
@@ -10,18 +10,19 @@ class HelpView(BaseView):
     def group(self, message):
         self.bot.reply_to(
             message,
-            "Hi, I'm the bot that will help you organize your secret "
+            "Hi, I'm Amigo and I will help you organize your secret "
             "santa event.\n"
-            "/start - launch bot in group\n"
-            "/join - take part in the event\n"
-            "/help - show this message"
+            " - Write /start to create an event.\n"
+            " - If you want to join the event, then write /join.\n"
+            f" - To fill form for this group, write /join and {self.get_link_to_me('write to me')} "
+            "in private messages.\n"
+            " - To send forms to members, write /send.",
+            parse_mode="HTML"
         )
 
     def private(self, message):
         self.bot.reply_to(
             message,
             "hello amigo\n"
-            "/start - start bot in private chat\n"
-            "/edit - fill in the data\n"
-            "/help - show this message"
+            " - Write /edit to fill in the data for any chat"
         )
