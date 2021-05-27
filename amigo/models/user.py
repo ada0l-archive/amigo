8a99fef89cf70418a0b15eab5ee7397fc6f8e4e0
andrey <andreika.varfolomeev@yandex.ru>
2021-05-27 23:55:23 +1000

    username is nullable

--- amigo/models/user.py
+++ amigo/models/user.py
@@ -6,7 +6,7 @@ from amigo.models.base import BaseModel
 class User(BaseModel):
     __tablename__ = 'users'
     telegram_id = Column(Integer, nullable=False)
-    username = Column(Text, nullable=False)
+    username = Column(Text, nullable=True)
 
     def __init__(self, telegram_id, username):
         self.telegram_id = telegram_id
